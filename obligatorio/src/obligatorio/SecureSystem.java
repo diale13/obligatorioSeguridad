d8e8956417d1eeccc45d7862f71259e95bdda080
Diego Franggi <diegofranggi@gmail.com>
2020-11-30 22:28:43 -0300

    lectura de archivo

--- obligatorio/src/obligatorio/SecureSystem.java
+++ obligatorio/src/obligatorio/SecureSystem.java
@@ -1,21 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package obligatorio;
 
-/**
- *
- * @author Cono
- */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
 public class SecureSystem {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        try {
+            String filePath = scan.nextLine();
+            BufferedReader in = new BufferedReader(new FileReader(filePath));
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+            in.close();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
-    
+
 }
